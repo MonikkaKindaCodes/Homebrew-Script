37efb6bf70b191ef4fa61b30f670536b1113b777
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-06-23 13:01:30 -0500

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# Homebrew Script for OSX
-# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
-
-echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Homebrew Office Script for OSX
 
 echo "Installing brew cask..."
 brew tap homebrew/cask
